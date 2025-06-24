58bdc8a75db653a754f5eccaafae7e86d096c7b0
nayon117 <hasibul.nayon1@gmail.com>
2025-06-24 16:59:27 +0600

    solve redirect problem

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useTitle from "../../hooks/useTitle";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
@@ -10,17 +10,26 @@ const SignUp = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm()
 
-  const {createUser} = useContext(AuthContext);
+  const {createUser,updateUserProfile} = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const onSubmit = (data) => {
     createUser(data.email,data.password)
     .then(res=>{
       const loggedUser = res.user;
       console.log(loggedUser);
-      toast.success("Signup Successfull")
+      updateUserProfile(data.name,data.photoURL)
+      .then(()=>{
+        console.log('user profile updated')
+        reset();
+        toast.success("Signup Successfull");
+        navigate('/');
+      })
+      .catch(err=>console.log(err));
     })
   }
   return (
@@ -41,6 +50,11 @@ const SignUp = () => {
               <label className="label">Name</label>
               <input type="text" {...register("name",{ required: true })} className="input" placeholder="your name" />
               {errors.name && <span className="text-red-600">Name is required</span>}
+
+              {/* photo */}
+              <label className="label">Photo URL</label>
+              <input type="text" {...register("photoURL",{ required: true })} className="input" placeholder="photoURL" />
+              {errors.photoURL && <span className="text-red-600">photoURL is required</span>}
 
               {/* // email */}
               <label className="label">Email</label>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,12 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -10,22 +17,29 @@ const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   // create user
-  const createUser = (email,password) =>{
+  const createUser = (email, password) => {
     setLoading(true);
-    return createUserWithEmailAndPassword(auth,email,password);
-  }
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
   // login user
-  const signIn = (email,password) =>{
+  const signIn = (email, password) => {
     setLoading(true);
-    return signInWithEmailAndPassword(auth,email,password);
-  }
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   // logout
   const logOut = () => {
     setLoading(true);
     return signOut(auth);
-  }
+  };
+
+  const updateUserProfile = (name,photo) => {
+   return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    })
+  };
 
   // subscription
   useEffect(() => {
@@ -41,9 +55,12 @@ const AuthProvider = ({ children }) => {
     loading,
     createUser,
     signIn,
-    logOut
+    logOut,
+    updateUserProfile
   };
 
-  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 export default AuthProvider;
